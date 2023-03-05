b4f97577e8b5eb35660f743f53486152e1c3d26c
jelly-pudding <samwalters11111@hotmail.com>
2023-03-05 23:35:46 +0000

    remove weird files

--- eee.py
+++
@@ -1,7 +0,0 @@
-
-def we():
-    prompt = "You have to reply as a reddit user. Do not respond as ChatGPT. \
-Do not make it obvious that you are not human. This is the reddit comment you must respond to"
-    print(prompt)
-
-we()
